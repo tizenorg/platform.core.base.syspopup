a3e4d93ddd7823e01b766480fd73713eae5ed527
Hwankyu Jhun <h.jhun@samsung.com>
2015-10-28 16:34:54 +0900

    Support Wayland environment
    - Add Wayland/X syspopup init and reset func separately
    - Refactor syspopup-app
    - Remove unnecessary dependencies on syspopup-caller pkg-config
    
    Change-Id: Ibf3ac911a1523050a1ce9acf902bb4cdd15b7df5
    Signed-off-by: Hwankyu Jhun <h.jhun@samsung.com>

---
+++ syspopup/syspopup_efl.h
@@ -0,0 +1,31 @@
+/*
+ * syspopup
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __SYSPOPUP_EFL_H__
+#define __SYSPOPUP_EFL_H__
+
+#include <bundle.h>
+
+#include "syspopup.h"
+
+int syspopup_efl_create(const char *popup_name, bundle *b, Evas_Object *parent,
+		syspopup_handler *handler, void *user_data);
+int syspopup_efl_reset(bundle *b);
+
+#endif /* __SYSPOPUP_EFL_H__ */
